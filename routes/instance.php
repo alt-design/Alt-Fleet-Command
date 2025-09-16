2ce30ca6be614d1728469a75a5bbdffbfc56dedd
benammi <benammi@alt-design.net>
2025-09-16 14:28:29 +0100

    [DEV-1352] Feature: formatting / users endpoints behind auth middleware

--- routes/instance.php
+++ routes/instance.php
@@ -2,22 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('oauth')->group( function () {
-    Route::get(
-        '/redirect',
-        [AltDesign\FleetCommand\Http\Controllers\OAuthController::class, 'redirect']
-    )->name('alt-fleet-cmd.oauth.redirect');
+Route::prefix('oauth')
+    ->group( function () {
+        Route::get(
+            '/redirect',
+            [AltDesign\FleetCommand\Http\Controllers\OAuthController::class, 'redirect']
+        )->name('alt-fleet-cmd.oauth.redirect');
 
-    Route::get(
-        '/callback',
-        [AltDesign\FleetCommand\Http\Controllers\OAuthController::class, 'callback']
-    )->name('alt-fleet-cmd.oauth.callback');
-});
+        Route::get(
+            '/callback',
+            [AltDesign\FleetCommand\Http\Controllers\OAuthController::class, 'callback']
+        )->name('alt-fleet-cmd.oauth.callback');
+    });
 
-Route::prefix('alt-fleet-cmd/users')->group( function () {
-    Route::post(
-        '/create',
-        [AltDesign\FleetCommand\Http\Controllers\Instance\UserController::class, 'create']
-    )->name('alt-fleet-cmd.users.create');
-});
+Route::prefix('alt-fleet-cmd/users')
+    ->middleware('validate.central')
+    ->group( function () {
+        Route::post(
+            '/create',
+            [AltDesign\FleetCommand\Http\Controllers\Instance\UserController::class, 'create']
+        )->name('alt-fleet-cmd.users.create');
+    });
 
