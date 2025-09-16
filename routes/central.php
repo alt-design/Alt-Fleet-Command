40cf94bba2b5ec4f0ef8b8f56b70767e05906d21
benammi <benammi@alt-design.net>
2025-09-16 17:32:24 +0100

    [DEV-1352] Feature: Passport Authed route for get-user

--- routes/central.php
+++ routes/central.php
@@ -1,5 +1,13 @@
 <?php
 
+use AltDesign\FleetCommand\Http\Controllers\Central\OAuthController;
 use Illuminate\Support\Facades\Route;
 
-//
+Route::prefix('alt-fleet-cmd/oauth')
+    ->middleware(config('alt-fleet-cmd.central.passport_auth_guard'))
+    ->group(function () {
+    Route::get(
+        '/get-user',
+        [OAuthController::class, 'getUser']
+    );
+});
