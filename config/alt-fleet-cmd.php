e707f41b1376f63a3ceb5020bc763d005202cdd5
benammi <benammi@alt-design.net>
2025-09-16 10:50:57 +0100

    [DEV-1352] Fix : config callback url fixed

--- config/alt-fleet-cmd.php
+++ config/alt-fleet-cmd.php
@@ -9,6 +9,6 @@ return [
         'client_id' => env('FLEET_COMMAND_OAUTH_CLIENT_ID', null),
         'client_secret' => env('FLEET_COMMAND_OAUTH_CLIENT_SECRET', null),
         'host' => env('FLEET_COMMAND_CENTRAL_URL'),
-        'redirect' => Str::finish(env('APP_URL'), '/') . 'auth/callback',
+        'redirect' => Str::finish(env('APP_URL'), '/') . 'oauth/callback',
     ],
 ];
