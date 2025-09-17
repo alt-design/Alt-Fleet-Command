16e874cafa6a3f408b3cd322e1dfe03ba029da12
benammi <benammi@alt-design.net>
2025-09-17 17:33:32 +0100

    [DEV-1352] Refactor: getUser() relocated from OAuthController renamed to fetch() and now checks for an InstanceUser to handle auth.

--- src/Http/Controllers/Central/UserController.php
+++ src/Http/Controllers/Central/UserController.php
@@ -4,7 +4,59 @@ declare(strict_types=1);
 
 namespace AltDesign\FleetCommand\Http\Controllers\Central;
 
+use AltDesign\FleetCommand\DTO\UserDTO;
+use AltDesign\FleetCommand\Models\Instance;
+use AltDesign\FleetCommand\Models\InstanceUser;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
 class UserController
 {
+    public function fetch(Request $request): JsonResponse
+    {
+        // Validate the incoming request data.
+        $validatedData = $request->validate([
+            'instance_url' => 'required|url'
+        ]);
 
+        // Get the authenticated user and convert it to a UserDTO instance.
+        $user = Auth::user();
+        $userDTO = UserDTO::make(
+            id: $user->id,
+            name: $user->name,
+            email: $user->email
+        );
+
+
+        $decodedInstanceUrl = urldecode($validatedData['instance_url']);
+        $strippedURL = Str::after($decodedInstanceUrl, '://');
+
+        // If the user is not authenticated, return a 401 Unauthorized response.
+        $instance = Instance::where('base_url', Str::finish('http://' . $strippedURL, '/'))
+            ->orWhere('base_url', Str::finish('https://' . $strippedURL, '/'))
+            ->first();
+        if (!$instance) {
+            return response()->json([
+                'message' => 'Invalid Instance URL.',
+            ], 400);
+        }
+
+        // Check if the user is authorized to access the instance.
+        $instanceUser = InstanceUser::getUserInstance(
+            user: $userDTO,
+            instance: $instance,
+        );
+
+        if (!$instanceUser) {
+            return response()->json([
+                'message' => 'Instance User record not found.',
+            ], 401);
+        }
+
+        return response()->json($user);
+    }
 }
