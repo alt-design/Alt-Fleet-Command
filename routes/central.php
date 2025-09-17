4dfe56b67b22fd0cc25c645c67900e732c329b65
benammi <benammi@alt-design.net>
2025-09-17 17:34:26 +0100

    [DEV-1352] Refactor: switch get-user route to use the refactored UserController method

--- routes/central.php
+++ routes/central.php
@@ -1,6 +1,6 @@
 <?php
 
-use AltDesign\FleetCommand\Http\Controllers\Central\OAuthController;
+use AltDesign\FleetCommand\Http\Controllers\Central\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('alt-fleet-cmd/oauth')
@@ -8,6 +8,6 @@ Route::prefix('alt-fleet-cmd/oauth')
     ->group(function () {
     Route::get(
         '/get-user',
-        [OAuthController::class, 'getUser']
+        [UserController::class, 'fetch']
     );
 });
