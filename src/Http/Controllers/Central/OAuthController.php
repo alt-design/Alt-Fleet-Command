aa2fa06565487adab5f02b9a46be143aa6a3476b
benammi <benammi@alt-design.net>
2025-09-17 17:32:05 +0100

    [DEV-1352] Refactor: remove getUser() method (relocated)

--- src/Http/Controllers/Central/OAuthController.php
+++ src/Http/Controllers/Central/OAuthController.php
@@ -6,17 +6,7 @@ namespace AltDesign\FleetCommand\Http\Controllers\Central;
 
 
 
-use AltDesign\FleetCommand\Models\OAuthToken;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Str;
-
 class OAuthController
 {
-    public function getUser(Request $request): mixed
-    {
-        return response()->json(Auth::user());
-    }
+
 }
