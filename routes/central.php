97d0fed9f247c790d4696484c1c44121487b021b
benammi <benammi@alt-design.net>
2025-11-18 14:29:38 +0000

    [DEV-2071] Feature: Provisioning route

--- routes/central.php
+++ routes/central.php
@@ -1,13 +1,22 @@
 <?php
 
+use AltDesign\FleetCommand\Http\Controllers\Central\ProvisioningController;
 use AltDesign\FleetCommand\Http\Controllers\Central\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('alt-fleet-cmd/oauth')
-    ->middleware(config('alt-fleet-cmd.central.passport_auth_guard'))
-    ->group(function () {
-    Route::get(
-        '/get-user',
-        [UserController::class, 'fetch']
-    );
+Route::prefix('alt-fleet-cmd')->group(function () {
+    Route::prefix('oauth')
+        ->middleware(config('alt-fleet-cmd.central.passport_auth_guard'))
+        ->group(function () {
+            Route::get(
+                '/get-user',
+                [UserController::class, 'fetch']
+            );
+        });
+
+    // Provisioning route protected by shared secret verification inside controller
+    Route::post(
+        '/provision',
+        [ProvisioningController::class, 'provision']
+    )->name('alt-fleet-cmd.provision');
 });
