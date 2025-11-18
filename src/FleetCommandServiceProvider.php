03139402ed1e0cf5b3702b3065fc9a08fe37db69
benammi <benammi@alt-design.net>
2025-11-18 15:28:41 +0000

    [DEV-2071] Refactor: Remove unnecessary try catch

--- src/FleetCommandServiceProvider.php
+++ src/FleetCommandServiceProvider.php
@@ -82,23 +82,19 @@ class FleetCommandServiceProvider extends ServiceProvider
 
     public function loadEnvironment(): self
     {
-        try {
-            if (Schema::hasTable('environments')) {
-                $map = [
-                    'FLEET_COMMAND_OAUTH_CLIENT_ID' => 'alt-fleet-cmd.oauth.client_id',
-                    'FLEET_COMMAND_OAUTH_CLIENT_SECRET' => 'alt-fleet-cmd.oauth.client_secret',
-                    'FLEET_COMMAND_INSTANCE_API_KEY' => 'alt-fleet-cmd.instance.api_key',
-                ];
+        if (Schema::hasTable('environments')) {
+            $map = [
+                'FLEET_COMMAND_OAUTH_CLIENT_ID' => 'alt-fleet-cmd.oauth.client_id',
+                'FLEET_COMMAND_OAUTH_CLIENT_SECRET' => 'alt-fleet-cmd.oauth.client_secret',
+                'FLEET_COMMAND_INSTANCE_API_KEY' => 'alt-fleet-cmd.instance.api_key',
+            ];
 
-                foreach ($map as $envKey => $configKey) {
-                    $val = EnvironmentModel::getValue($envKey);
-                    if ($val !== null && $val !== '') {
-                        Config::set($configKey, $val);
-                    }
+            foreach ($map as $envKey => $configKey) {
+                $val = EnvironmentModel::getValue($envKey);
+                if ($val !== null && $val !== '') {
+                    Config::set($configKey, $val);
                 }
             }
-        } catch (\Throwable $e) {
-            // Silently ignore if DB not ready (e.g. before migrations)
         }
 
         return $this;
